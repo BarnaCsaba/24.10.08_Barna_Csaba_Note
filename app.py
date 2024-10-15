659877c592eab5f9b8ea43b37b833e6221814491
BarnaCsaba <barna.csaba@diak.szbi-pg.hu>
2024-10-15 10:24:49 +0200

    valami

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 products = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return render_template("index.html")
+
     
-    if request.method == 'POST':
+    
+@app.route('/submit_products', methods=['POST'])
+def submit_products():
         name = request.form.get('product_name')
         products.append(name)
-    return render_template("index.html", products_=products)
-
+        return redirect(url_for('index'))
     
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
-
